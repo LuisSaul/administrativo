9ecb81702d9b0647f23fa92d73d9fd0ff449ab1f
Juda Alector Vallejo Herrera <juda_alector@hotmail.com>
2018-11-21 12:04:15 -0600

    error

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body class="mb-5 pb-5">  
   <div class="container-fluid">
     <div class="row content-form">
-      <div class="col-5 pl-5 login">
+      <div class="col-5 pl-5">
         <h1 clas=""> Administradores </h1>
         <div class="form-group">
           <label clas="">Usuario:</label>
@@ -30,12 +30,9 @@
     </div>
   </div>
     
-  <!-- JS SCRIPTS -->
   <script src="./lib/popper.js"></script>
   <script src="./lib/jquery.js"></script>
   <script src="./lib/bootstrap.js"></script>
-  
-  <!-- Own JS -->
   <script src="./js/login.js"></script>
 </body>
 </html>
